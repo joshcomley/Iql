75caf032c0e85dce510835c6185702d226f28993
Josh Comley <joshcomley@googlemail.com>
2017-11-30 03:05:17 +0000

    Spacing and version

--- Code/Tests/Iql.Tests/UnitTest1.cs
+++ Code/Tests/Iql.Tests/UnitTest1.cs
@@ -56,6 +56,7 @@ namespace Iql.Tests
             AppDbContext.InMemoryDb.ClientTypes.Clear();
             Db = new AppDbContext();
         }
+
         private static ClientTypes AddClientTypes()
         {
             // Create two new client types
