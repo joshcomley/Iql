2ea20695de020c44248503db8da1d1eb69a6a9ba
Josh Comley <joshcomley@googlemail.com>
2017-12-10 20:42:41 +0000

    More tests

--- Code/Tests/Iql.Tests/Tests/SaveChangesTests.cs
+++ Code/Tests/Iql.Tests/Tests/SaveChangesTests.cs
@@ -41,7 +41,7 @@ namespace Iql.Tests.Tests
                 queue = Db.DataStore.GetQueue().ToList();
                 Assert.AreEqual(0, queue.Count);
             }
-            
+
             // Should be no changes so far
             AssertQueueEmpty();
 
@@ -80,11 +80,10 @@ namespace Iql.Tests.Tests
 
             var clients = await Db.Clients.ToList();
             var client1 = clients.Single(c => c.Id == 1);
-            List<IQueuedOperation> queue = null;
 
             void AssertDescriptionOnlyQueued(string newDescription)
             {
-                queue = Db.DataStore.GetQueue().ToList();
+                var queue = Db.DataStore.GetQueue().ToList();
                 Assert.AreEqual(1, queue.Count);
                 var update = queue[0] as QueuedUpdateEntityOperation<Client>;
                 Assert.IsNotNull(update);
@@ -99,7 +98,7 @@ namespace Iql.Tests.Tests
 
             void AssertBothChangesQueued(string newName, string newDescription)
             {
-                queue = Db.DataStore.GetQueue().ToList();
+                var queue = Db.DataStore.GetQueue().ToList();
                 Assert.AreEqual(1, queue.Count);
                 var update = queue[0] as QueuedUpdateEntityOperation<Client>;
                 Assert.IsNotNull(update);
@@ -114,12 +113,6 @@ namespace Iql.Tests.Tests
                 var descriptionChange = propertyChanges.Single(p => p.Property.Name == nameof(Client.Description));
                 Assert.AreEqual(null, descriptionChange.OldValue);
                 Assert.AreEqual(newDescription, descriptionChange.NewValue);
-            }
-
-            void AssertQueueEmpty()
-            {
-                queue = Db.DataStore.GetQueue().ToList();
-                Assert.AreEqual(0, queue.Count);
             }
 
             // Should be no changes so far
@@ -151,6 +144,57 @@ namespace Iql.Tests.Tests
             var dbClient = AppDbContext.InMemoryDb.Clients.Single(c => c.Id == client1.Id);
             Assert.AreEqual(dbClient.Name, "A new name 2");
             Assert.AreEqual(dbClient.Description, "A new description");
+        }
+
+        public void AssertQueueEmpty()
+        {
+            var queue = Db.DataStore.GetQueue().ToList();
+            Assert.AreEqual(0, queue.Count);
+        }
+
+        [TestMethod]
+        public async Task ChangeTwoUnrelatedEntitiesAndSave()
+        {
+            // Set up
+            AppDbContext.InMemoryDb.Clients.Add(new Client { Id = 1, Name = "Client 1" });
+            AppDbContext.InMemoryDb.Clients.Add(new Client { Id = 2, Name = "Client 2" });
+            AppDbContext.InMemoryDb.Sites.Add(new Site { Id = 1, Name = "Site 1" });
+            AppDbContext.InMemoryDb.Sites.Add(new Site { Id = 2, Name = "Site 2" });
+
+            var client1 = await Db.Clients.WithKey(1);
+            var sites = await Db.Sites.ToList();
+            var site1 = sites.Single(s => s.Id == 1);
+
+            site1.Name = "Site 1 - changed";
+            client1.Name = "Client 1 - changed";
+
+            var queue = Db.DataStore.GetQueue().ToList();
+            Assert.AreEqual(2, queue.Count);
+
+            var siteChange = queue.Single(
+                    q =>
+                        q.Operation is UpdateEntityOperation<Site> &&
+                        (q.Operation as UpdateEntityOperation<Site>).Entity == site1)
+                        .Operation
+                as UpdateEntityOperation<Site>;
+            Assert.AreEqual(1, siteChange.EntityState.ChangedProperties.Count);
+            Assert.AreEqual(nameof(Site.Name), siteChange.EntityState.ChangedProperties[0].Property.Name);
+
+            var clientChange = queue.Single(
+                    q =>
+                        q.Operation is UpdateEntityOperation<Client> &&
+                        (q.Operation as UpdateEntityOperation<Client>).Entity == client1)
+                    .Operation
+                as UpdateEntityOperation<Client>;
+            Assert.AreEqual(1, clientChange.EntityState.ChangedProperties.Count);
+            Assert.AreEqual(nameof(Client.Name), clientChange.EntityState.ChangedProperties[0].Property.Name);
+
+            await Db.SaveChanges();
+
+            AssertQueueEmpty();
+
+            Assert.AreEqual("Site 1 - changed", AppDbContext.InMemoryDb.Sites.Single(s => s.Id == site1.Id).Name);
+            Assert.AreEqual("Client 1 - changed", AppDbContext.InMemoryDb.Clients.Single(s => s.Id == client1.Id).Name);
         }
     }
 }
