ff8026b35ba9e19175649d5660139400ed738675
Josh Comley <joshcomley@googlemail.com>
2017-12-21 17:30:54 +0000

    Test tracking 2,000 objects

---
+++ Code/Tests/Iql.Tests/Tests/TrackingTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Iql.Tests.Tests
+{
+    [TestClass]
+    public class TrackingTests : TestsBase
+    {
+        [TestMethod]
+        public void TestTrackingSpeed()
+        {
+            var date = DateTime.Now;
+            for (var i = 0; i < 1000; i++)
+            {
+                var clientType = new ClientType
+                {
+                    Id = i + 1,
+                    Name = "Client type " + (i + 1),
+                };
+                clientType.Clients.AddRange(new[]
+                {
+                    new Client {Id = i+1, Name = "Client " + (i + 1)}
+                });
+                Db.ClientTypes.Add(clientType);
+            }
+            var time = DateTime.Now - date;
+            var seconds = time.TotalSeconds;
+        }
+    }
+}
