013e1aad55986c7301f5f7f2b218d913c53bb28e
Josh Comley <joshcomley@googlemail.com>
2018-12-14 20:35:48 +0000

    Added validation logger and property detail

---
+++ Code/Core/Iql.Data/Logging/ValidationLogger.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Iql.Data.Crud;
+using Iql.Entities;
+using Iql.Entities.Validation.Validation;
+
+namespace Iql.Data.Logging
+{
+    public class ValidationLogger
+    {
+        private StringBuilder _sb;
+
+        public ValidationLogger()
+        {
+            _sb = new StringBuilder();
+        }
+
+        public void Clear()
+        {
+            _sb = new StringBuilder();
+        }
+
+        public string ParseResults(IEntityCrudResult[] results)
+        {
+            for (var i = 0; i < results.Length; i++)
+            {
+                ParseValidationResult(results[i]);
+            }
+            var str = _sb.ToString();
+            if (str == "")
+            {
+                str = "None";
+            }
+            return "Validation errors:\n" + str;
+        }
+
+        public void ParseValidationResult(IEntityCrudResult validationResult)
+        {
+            var config = EntityConfigurationBuilder.FindConfigurationForEntityType(
+                validationResult.LocalEntity.GetType());
+            _sb.AppendLine($"{config.FriendlyName}:");
+            _sb.AppendLine($"Key: {config.GetCompositeKeyString(validationResult.LocalEntity)}");
+            for (var i = 0; i < validationResult.EntityValidationResults.Count; i++)
+            {
+                ParseEntityValidationResult(validationResult.EntityValidationResults[i]);
+            }
+        }
+
+        private void ParseEntityValidationResult(IEntityValidationResult validationResult)
+        {
+            if (validationResult == null)
+            {
+                return;
+            }
+
+            ParseValidationFailures(validationResult.ValidationFailures);
+
+            if (validationResult.PropertyValidationResults != null)
+            {
+                foreach (var propertyResult in validationResult.PropertyValidationResults)
+                {
+                    ParsePropertyValidationResult(propertyResult);
+                }
+            }
+            if (validationResult.RelationshipValidationResults != null)
+            {
+                foreach (var relationshipResult in validationResult.RelationshipValidationResults)
+                {
+                    ParsePropertyValidationResult(relationshipResult);
+                }
+            }
+        }
+
+        private void ParsePropertyValidationResult(IPropertyValidationResult validationResult)
+        {
+            if (validationResult == null)
+            {
+                return;
+            }
+            _sb.AppendLine($"Property - {validationResult.Property.PropertyName}:");
+            ParseValidationFailures(
+                validationResult.ValidationFailures);
+        }
+
+        private void ParseValidationFailures(IReadOnlyList<ValidationError> validationFailures)
+        {
+            if (validationFailures == null)
+            {
+                return;
+            }
+            for (var i = 0; i < validationFailures.Count; i++)
+            {
+                ParseValidationFailure(validationFailures[i]);
+            }
+        }
+
+        private void ParseValidationFailure(ValidationError validationFailure)
+        {
+            _sb.AppendLine($"{validationFailure.Key}:");
+            _sb.AppendLine($"{validationFailure.Message}");
+        }
+    }
+}
---
+++ Code/Core/Iql.Data/Rendering/PropertyDetail.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Iql.Data.Context;
+using Iql.Data.Extensions;
+using Iql.Entities;
+using Iql.Entities.NestedSets;
+using Iql.Entities.PropertyGroups.Dates;
+using Iql.Entities.Relationships;
+
+namespace Iql.Data.Rendering
+{
+    public class PropertyDetail
+    {
+        private PropertyDetail(IPropertyContainer property)
+        {
+            Property = property;
+            PropertyAsPropertyCollection = property as PropertyCollection;
+            PropertyAsRelationship = property as RelationshipDetailBase;
+            PropertyAsPropertyGroup = property as IPropertyGroup;
+            PropertyAsCoreProperty = property as IProperty;
+            PropertyAsSimpleProperty = property as ISimpleProperty;
+            PropertyAsDateRange = property as IDateRange;
+            PropertyAsNestedSet = property as INestedSet;
+            PropertyAsEntityConfiguration = property as IEntityConfiguration;
+
+            var kind = ResolveKind(property, out var canShow);
+            Kind = kind;
+            CanShow = canShow;
+        }
+
+        private static readonly Dictionary<IPropertyContainer, PropertyDetail> Mapping =
+            new Dictionary<IPropertyContainer, PropertyDetail>();
+        public static PropertyDetail For(IPropertyContainer property)
+        {
+            if (property == null)
+            {
+                return null;
+            }
+            if (!Mapping.ContainsKey(property))
+            {
+                Mapping[property] = new PropertyDetail(property);
+            }
+            return Mapping[property];
+        }
+        private static string ResolveKind(IPropertyContainer property, out bool canShow)
+        {
+            var kind = "unknown";
+            canShow = true;
+            var prop = property as PropertyBase;
+            var rel = property as RelationshipDetailBase;
+            if (prop != null)
+            {
+                switch (prop.TypeDefinition.Kind)
+                {
+                    case IqlType.Enum:
+                        kind = "enum";
+                        break;
+                    case IqlType.String:
+                        kind = "string";
+                        break;
+                    case IqlType.Date:
+                        kind = "date";
+                        break;
+                    case IqlType.Boolean:
+                        kind = "boolean";
+                        break;
+                    case IqlType.Integer:
+                    case IqlType.Decimal:
+                        kind = "number";
+                        break;
+                    case IqlType.GeometryPolygon:
+                    case IqlType.GeographyPolygon:
+                        kind = "geopolygon";
+                        break;
+                    case IqlType.GeometryPoint:
+                    case IqlType.GeographyPoint:
+                        kind = "geopoint";
+                        break;
+                }
+
+                if (prop.Kind.HasFlag(PropertyKind.Key))
+                {
+                    kind = "key";
+                    canShow = false;
+                }
+                else if (prop.Kind.HasFlag(PropertyKind.RelationshipKey))
+                {
+                    kind = "relationship-key";
+                    canShow = false;
+                }
+                else if (prop.Kind == PropertyKind.Relationship)
+                {
+                    kind = "relationship";
+                }
+            }
+            else if (rel != null && rel.RelationshipSide == RelationshipSide.Target)
+            {
+                kind = "relationship-target";
+            }
+            else if (rel != null && rel.RelationshipSide == RelationshipSide.Source)
+            {
+                kind = "relationship-source";
+            }
+            else
+            {
+                kind = "group";
+            }
+
+            return kind;
+        }
+
+        public bool IsCollection => IsEntityConfiguration || IsPropertyCollection || IsBasicProperty;
+        public bool IsPropertyCollection => PropertyAsPropertyCollection != null;
+        public bool IsPropertyGroup => PropertyAsPropertyGroup != null;
+        public bool IsCoreProperty => PropertyAsCoreProperty != null;
+        public bool IsEntityConfiguration => PropertyAsEntityConfiguration != null;
+
+        public bool IsHorizontalPropertyCollection => IsPropertyCollection &&
+                                                      PropertyAsPropertyCollection.ContentAlignment ==
+                                                      ContentAlignment.Horizontal;
+
+        public bool IsSimpleProperty => IsPropertyGroup && PropertyAsPropertyGroup.Kind.HasFlag(PropertyKind.Property);
+        public bool IsBasicProperty => IsPropertyGroup && PropertyAsPropertyGroup.Kind.HasFlag(PropertyKind.Property);
+
+        public bool IsGeographic => IsCoreProperty && PropertyAsCoreProperty.TypeDefinition != null &&
+                                    PropertyAsCoreProperty.TypeDefinition.Type == typeof(IqlPointExpression);
+
+        public bool IsNestedSet => PropertyAsNestedSet != null;
+        public bool IsDateRange => PropertyAsDateRange != null;
+
+        public bool IsTargetRelationship =>
+            IsRelationship && PropertyAsRelationship.RelationshipSide == RelationshipSide.Target;
+
+        public bool IsSourceRelationship =>
+            IsRelationship && PropertyAsRelationship.RelationshipSide == RelationshipSide.Source;
+
+        public bool IsRelationship => PropertyAsRelationship != null;
+
+        public ISimpleProperty PropertyAsSimpleProperty { get; }
+        public IEntityConfiguration PropertyAsEntityConfiguration { get; }
+        public PropertyCollection PropertyAsPropertyCollection { get; }
+        public IPropertyGroup PropertyAsPropertyGroup { get; }
+        public IProperty PropertyAsCoreProperty { get; }
+        public INestedSet PropertyAsNestedSet { get; }
+        public IDateRange PropertyAsDateRange { get; }
+        public RelationshipDetailBase PropertyAsRelationship { get; }
+
+        public IPropertyContainer Property { get; set; }
+
+        // Metadata snapshot
+        public string Kind { get; set; }
+        public bool CanShow { get; set; }
+        public bool CanEdit { get; set; }
+        public PropertyDetail[] ChildProperties { get; set; }
+
+        public async Task UpdateAsync(object entity, IDataContext dataContext)
+        {
+            CanEdit = !IsSimpleProperty || await PropertyAsCoreProperty.IsReadOnlyAsync(entity, dataContext);
+            var entityConfiguration = Property as IEntityConfiguration;
+            var allProperties =
+                entityConfiguration != null
+                    ? entityConfiguration.GetDisplayConfiguration(DisplayConfigurationKind.Edit)
+                    : Property.GetGroupProperties();
+            // If there is no property order configuration, just use all simple properties from this type
+            if (entityConfiguration != null && (allProperties == null || allProperties.Length == 0))
+                allProperties = entityConfiguration.Properties.ToArray();
+            var properties = new List<IPropertyGroup>();
+            var filteredProperties = new List<IPropertyGroup>();
+            for (var i = 0; i < allProperties.Length; i++)
+            {
+                var property = allProperties[i];
+                var rel = property as RelationshipDetailBase;
+                if (property.Kind.HasFlag(PropertyKind.Property))
+                {
+                    if (property.Kind.HasFlag(PropertyKind.Count))
+                    {
+                        continue;
+                    }
+                    var simpleProperty = property as IProperty;
+                    if (simpleProperty != null &&
+                        simpleProperty.Relationship != null &&
+                        simpleProperty.Relationship.ThisIsTarget &&
+                        await simpleProperty.Relationship.OtherEnd.Property.IsReadOnlyAsync(entity, dataContext))
+                    {
+                        continue;
+                    }
+                }
+                else if (rel != null && rel.RelationshipSide == RelationshipSide.Target && !rel.AllowInlineEditing)
+                {
+                    continue;
+                }
+
+                properties.Add(property);
+            }
+
+            for (var i = 0; i < properties.Count; i++)
+            {
+                var propertyGroup = properties[i];
+                var detail = For(properties[i]);
+                if (detail == null)
+                {
+                    continue;
+                }
+
+                await detail.UpdateAsync(entity, dataContext);
+
+                var relationshipDetail = propertyGroup as RelationshipDetailBase;
+                if (detail.Kind == "group" &&
+                    relationshipDetail != null &&
+                    relationshipDetail.Property.EditKind != PropertyEditKind.Edit)
+                {
+                    continue;
+                }
+
+                if (detail.CanShow)
+                {
+                    filteredProperties.Add(propertyGroup);
+                }
+            }
+            ChildProperties = filteredProperties.Select(For).ToArray();
+        }
+    }
+}
