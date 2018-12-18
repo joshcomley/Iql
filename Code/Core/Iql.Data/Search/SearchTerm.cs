63a4ba60a78da58682a73b77e226eda4e828dce6
Josh Comley <joshcomley@googlemail.com>
2018-12-18 12:45:57 +0000

    Added search term extractor

---
+++ Code/Core/Iql.Data/Search/SearchTerm.cs
@@ -0,0 +1,11 @@
+﻿namespace Iql.Data.Search
+{
+    public class SearchTerm
+    {
+        public string Value { get; set; }
+        public SearchTerm(string value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ Code/Tests/Iql.Tests/Tests/Search/SearchTermExtractionTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Iql.Data.Search;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Iql.Tests.Tests.Search
+{
+    [TestClass]
+    public class SearchTermExtractionTests
+    {
+        [TestMethod]
+        public void TestExtractSearchTerms()
+        {
+            var extractor = new SearchTermExtractor();
+            var terms = extractor.ExtrapolateSearchTerms(
+                    @"I am ""here to"" test this ""function"" out on ""you")
+                .Select(_ => _.Value).ToArray();
+            Assert.AreEqual(9, terms.Length);
+            var index = 0;
+            Assert.AreEqual("here to", terms[index++]);
+            Assert.AreEqual("function", terms[index++]);
+            Assert.AreEqual("I", terms[index++]);
+            Assert.AreEqual("am", terms[index++]);
+            Assert.AreEqual("test", terms[index++]);
+            Assert.AreEqual("this", terms[index++]);
+            Assert.AreEqual("out", terms[index++]);
+            Assert.AreEqual("on", terms[index++]);
+            Assert.AreEqual("\"you", terms[index++]);
+        }
+    }
+}
