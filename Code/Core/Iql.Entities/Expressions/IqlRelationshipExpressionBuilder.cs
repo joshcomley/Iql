0b51b5af42709cec3a0c917233be2d0585d93a43
Josh Comley <joshcomley@googlemail.com>
2019-03-13 15:20:19 +0000

    Migrated IqlEventManager and IqlRelationshipExpressionBuilder from Iql.Angular

---
+++ Code/Core/Iql.Entities/Expressions/IqlRelationshipExpressionBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Iql.Data.Queryable;
+
+namespace Iql.Entities.Expressions
+{
+    public class IqlRelationshipExpressionBuilder
+    {
+        public static IqlExpression BuildEntityQuery(IqlPropertyExpression propertyExpression, CompositeKey compositeKey) {
+            var expressions = new List<IqlExpression>();
+            for (var i = 0; i<compositeKey.Keys.Length; i++) {
+                var key = compositeKey.Keys[i];
+                var parentAsReference = propertyExpression.Parent as IqlReferenceExpression;
+                var newPropertyExpression = new IqlPropertyExpression(key.Name, parentAsReference);
+                expressions.Add(new IqlIsEqualToExpression(newPropertyExpression,
+                    new IqlLiteralExpression(key.Value, key.ValueType.Kind)));
+            }
+            return IqlQueryBuilder.And(expressions);
+        }
+
+    }
+}
---
+++ Code/Core/Iql.Events/IqlEventManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Iql.Events
+{
+    public class IqlEventManager : IDisposable
+    {
+        private readonly List<EventSubscription> _subscriptions = new List<EventSubscription>();
+
+        public void Dispose()
+        {
+            for (var i = 0; i < _subscriptions.Count; i++)
+            {
+                _subscriptions[i].Unsubscribe();
+            }
+        }
+
+        public EventSubscription Subscribe<T>(IEventSubscriber<T> subscriber, Action<T> action)
+        {
+            var sub = subscriber.Subscribe(action);
+            _subscriptions.Add(sub);
+            return sub;
+        }
+
+        public EventSubscription SubscribeAsync<T>(IAsyncEventSubscriber<T> subscriber, Func<T, Task> action)
+        {
+            var sub = subscriber.SubscribeAsync(action);
+            _subscriptions.Add(sub);
+            return sub;
+        }
+
+        //public class IqlEventService
+        //{
+        //    public entitySelected = new EventEmitter<IqlEntitySelectedEvent<any>>();
+        //    public entityHighlight = new EventEmitter<IqlEntityHighlightedEvent<any>>();
+
+        //    public highlight(entity: any, on: boolean)
+        //    {
+        //        this.entityHighlight.Emit(() => new IqlEntityHighlightedEvent(on, entity));
+        //    }
+
+        //    private dic = new Dictionary<any, IqlEventSubscriber>();
+        //    public getSubscriber(key: any) : IqlEventSubscriber {
+        //        if (!this.dic.ContainsKey(key)) {
+        //            this.dic.Set(key, new IqlEventSubscriber(this));
+        //        }
+        //        return this.dic.Get(key);
+        //    }
+        //}
+    }
+}
