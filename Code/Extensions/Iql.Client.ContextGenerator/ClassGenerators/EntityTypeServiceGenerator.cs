573be0fcbef366536f5162b00ab064ce3dc1b5c9
Josh Comley <joshcomley@googlemail.com>
2019-03-16 10:04:33 +0000

    Adding ConfigurationFor_[Type] to statically retrieve an entity configuration

--- Code/Extensions/Iql.Client.ContextGenerator/ClassGenerators/EntityTypeServiceGenerator.cs
+++ Code/Extensions/Iql.Client.ContextGenerator/ClassGenerators/EntityTypeServiceGenerator.cs
@@ -8,6 +8,7 @@ using Iql.OData.TypeScript.Generator.DataContext;
 using Iql.OData.TypeScript.Generator.Definitions;
 using Iql.OData.TypeScript.Generator.Models;
 using Iql.OData.TypeScript.Generator.Parsers;
+using Iql.Server.Serialization.Deserialization.EntityConfiguration;
 using GeneratedFile = Iql.OData.TypeScript.Generator.Models.GeneratedFile;
 
 namespace Iql.Client.ContextGenerator.ClassGenerators
@@ -49,6 +50,7 @@ namespace Iql.Client.ContextGenerator.ClassGenerators
                     foreach (var entitySetDefinition in _entitySetDefinitions)
                     {
                         GenerateType(entitySetDefinition.Type.Name);
+                        GenerateConfiguration(entitySetDefinition.Type.Name);
                         foreach (var property in entitySetDefinition.Type.Properties)
                         {
                             GenerateProperty(entitySetDefinition.Type.Name, property.Name);
@@ -59,9 +61,16 @@ namespace Iql.Client.ContextGenerator.ClassGenerators
             return File;
         }
 
+        private void GenerateConfiguration(string type)
+        {
+            var privateName = $@"_configurationFor_{type}";
+            AppendLine($"private {nameof(EntityConfiguration<object>)}<{type}> {privateName} = null;");
+            AppendLine($"public {nameof(EntityConfiguration<object>)}<{type}> ConfigurationFor_{type} => {privateName} = {privateName} ?? {nameof(EntityTypeService.Builder)}.{nameof(IEntityConfigurationBuilder.EntityType)}<{type}>();");
+        }
+
         private void GenerateType(string type)
         {
-            AppendLine($"public {nameof(Type)} TypeOf{type} => typeof({type});");
+            AppendLine($"public {nameof(Type)} TypeOf_{type} => typeof({type});");
         }
 
         private void GenerateProperty(string type, string property)
