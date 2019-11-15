c9c4c13a6c7778473a673cb2eece3057c84f0935
Josh Comley <Josh Comley>
2019-11-15 15:11:24 +0000

    Support for latest mocha

--- Code/Tests/Iql.Tests/Tests/InMemoryTests.cs
+++ Code/Tests/Iql.Tests/Tests/InMemoryTests.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Iql.Tests.Tests
 {
     [TestClass]
-    public class InMemoryTests : TestsBase
+    public abstract class InMemoryTests : TestsBase
     {
         [TestInitialize]
         public override void TestInitialize()
