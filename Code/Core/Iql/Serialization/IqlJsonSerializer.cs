cbd5eeb399a79b04fac3d6f728ecd6f2a1dc64d3
Josh Comley <joshcomley@googlemail.com>
2022-02-08 00:50:46 +0000

    Fix JSON serialisation

--- Code/Core/Iql/Serialization/IqlJsonSerializer.cs
+++ Code/Core/Iql/Serialization/IqlJsonSerializer.cs
@@ -7,20 +7,24 @@ namespace Iql.Serialization
     {
         public static string Serialize(object obj, IqlJsonSerializerSettings settings = null)
         {
+#if !TypeScript
             var converters = new List<JsonConverter>();
-            #if !TypeScript
             if (settings?.UseNumberConverter == true)
             {
                 converters.Add(new NumberConverter());
             }
-            #endif
-            return JsonConvert.SerializeObject(obj, new JsonSerializerSettings
-            {
-                NullValueHandling = settings == null 
-                    ? NullValueHandling.Include 
-                    : NullValueHandling.Ignore,
-                Converters = converters
-            });
+#endif
+            return JsonConvert.SerializeObject(obj
+#if !TypeScript
+                , new JsonSerializerSettings
+                {
+                    NullValueHandling = settings == null
+                        ? NullValueHandling.Include
+                        : NullValueHandling.Ignore,
+                    Converters = converters
+                }
+#endif
+            );
         }
     }
 }
